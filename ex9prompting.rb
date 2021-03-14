0e7ccdfe9996f00cb0f6cbcc3958102733681d2e
Gokulnarayanan32 <gokulnarayanan32@gmail.com>
2021-03-13 21:51:49 -0800

    ruby assignments

---
+++ ex9prompting.rb
@@ -0,0 +1,9 @@
+print "give me a number:"
+number = gets.chomp.to_f
+bigger = number * 100
+puts "bigger no is #{bigger}"
+
+print "give me an another no:"
+anotherno = gets.chomp.to_f
+smaller = number / 100
+puts "smaller no is #{smaller}"
